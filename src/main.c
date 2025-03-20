cc4774e0c29112c0990549749e27d5f4880cec4d
daniilgriga <daniilextra13@gmail.com>
2025-03-20 11:00:46 +0300

    Converter refactored...

--- src/main.c
+++ src/main.c
@@ -6,11 +6,11 @@ extern int MyPrintf (const char* a, ...);
 
 int main (void)
 {
-    int result = MyPrintf ("\n" "greeting = '%s'." "\n 245 %%d: '%d'\n 245 %%b: '%b'\n 245 %%o: '%o'\n 245 %%x: '%x'" "\n",
-                                "sup man, thats your debug:", 245, 245, 245, 245);
+    int result = MyPrintf ("\n" "greeting = '%s'." "\n 123 %%d: '%d'\n 123 %%b: '%b'\n 123 %%o: '%o'\n 123 %%x: '%x'" "\n",
+                                "sup man, thats your debug:", 123, 123, 123, 123);
     if (result == 666)
         fprintf (stderr,RED_TEXT(" Error in ") PURPLE_TEXT("%s: %d") "\n", __FILE__, __LINE__ - 2);
 
-    fprintf (stderr, "REAL PRINTF:\n 245 %%d: '%d'\n 245 %%b: '%b'\n 245 %%o: '%o'\n 245 %%x: '%x'" "\n", 245, 245, 245, 245);
+    fprintf (stderr, "REAL PRINTF:\n 123 %%d: '%d'\n 123 %%b: '%b'\n 123 %%o: '%o'\n 123 %%x: '%x'" "\n", 123, 123, 123, 123);
     return 0;
 }
